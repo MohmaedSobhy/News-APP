24d3cf9e13d2c72d7119ce2098bd699aed18d864
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-12-05 20:38:05 +0200

    home repository implementation

--- lib/Feature/home/data/repository/home_repository_implmentation.dart
+++ lib/Feature/home/data/repository/home_repository_implmentation.dart
@@ -1,6 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:news_app/Feature/home/data/model/news_model.dart';
 import 'package:news_app/Feature/home/data/repository/home_repository.dart';
+import 'package:news_app/core/API/api_servies.dart';
+import 'package:news_app/core/API/end_pointes.dart';
 import 'package:news_app/core/errors/fauiler_class.dart';
 
 class HomeRepositoryImplmentation implements HomeRepository {
@@ -13,13 +16,40 @@ class HomeRepositoryImplmentation implements HomeRepository {
   }
 
   @override
-  Future<Either<Failure, List<NewsModel>>> fetchAllNewes() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<NewsModel>>> fetchAllNewes() async {
+    try {
+      List<NewsModel> news = [];
+      var response = await ApiServices.getData(url: ApiEndPointes.allNews);
+      var data = response.data;
+      for (var item in data['articles']) {
+        news.add(NewsModel.formJson(item));
+      }
+      return Right(news);
+    } catch (error) {
+      if (error is DioException) {
+        return Left(ServerFailure.fromDioError(error));
+      }
+      return Left(ServerFailure(error.toString()));
+    }
   }
 
   @override
   Future<Either<Failure, List<NewsModel>>> fetchAllNewesByCategory(
-      {required String category}) {
-    throw UnimplementedError();
+      {required String category}) async {
+    try {
+      List<NewsModel> news = [];
+      var response = await ApiServices.getData(
+          url: "${ApiEndPointes.allNewsByCategory}$category");
+      var data = response.data;
+      for (var item in data['articles']) {
+        news.add(NewsModel.formJson(item));
+      }
+      return Right(news);
+    } catch (error) {
+      if (error is DioException) {
+        return Left(ServerFailure.fromDioError(error));
+      }
+      return Left(ServerFailure(error.toString()));
+    }
   }
 }
--- lib/core/API/end_pointes.dart
+++ lib/core/API/end_pointes.dart
@@ -2,6 +2,7 @@ abstract class ApiEndPointes {
   static const String apiKey = "1ad704bc419e4be5909e5fc3495a393a";
   static String baseUrl = "https://newsapi.org/v2/top-headlines";
   static String allNews = "$baseUrl?country=us&apiKey=$apiKey";
-  static String allNewsByCategory = "$baseUrl?country=us&apiKey=$apiKey";
+  static String allNewsByCategory =
+      "$baseUrl?country=us&apiKey=$apiKey&category=";
   //category=$category
 }
