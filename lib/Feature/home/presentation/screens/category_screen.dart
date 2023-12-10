a62d913c3ebccd56707e8dd497cfdb757cd2995e
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-12-10 14:07:01 +0200

    handle intenet connection
    handle intenet connection in category screen

--- lib/Feature/home/presentation/screens/category_screen.dart
+++ lib/Feature/home/presentation/screens/category_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:go_router/go_router.dart';
 import 'package:news_app/Feature/home/presentation/controller/news_by_category_cubit.dart/news_by_category_cubit.dart';
 import 'package:news_app/Feature/home/presentation/views/custome_title_app_bar.dart';
 import 'package:news_app/Feature/home/presentation/views/fetch_all_news_by_category_sucess.dart';
 import 'package:news_app/Feature/home/presentation/views/shimmer_all_news_category_view.dart';
+import 'package:news_app/core/widgets/fauiler_widget.dart';
+import 'package:news_app/core/widgets/no_internet_conncetion.dart';
 import 'package:news_app/core/widgets/no_news_forCategory.dart';
 
 class CategoryScreen extends StatelessWidget {
@@ -30,20 +33,37 @@ class CategoryScreen extends StatelessWidget {
             title: category,
           ),
         ),
-        body: BlocBuilder<NewsByCategoryCubit, NewsByCategoryState>(
-          builder: (context, state) {
-            if (state is FetchNewsByCategoryLoading) {
-              return const ShimmerNewsByCategoryView();
-            } else if (state is FetchNewsByCategorySucess) {
-              if (state.news.isEmpty) {
-                return const NoNewsForThisCategoryViews();
-              }
-              return FetchAllnewsByCategorySucess(news: state.news);
-            }
-            return Text("Hello $category");
+        body: OfflineBuilder(
+          connectivityBuilder: (BuildContext context,
+              ConnectivityResult connectivity, Widget child) {
+            return (connectivity != ConnectivityResult.none)
+                ? const CategoryBodyScreen()
+                : const NoInternetConnectionWidget();
           },
+          child: const NoInternetConnectionWidget(),
         ),
       ),
+    );
+  }
+}
+
+class CategoryBodyScreen extends StatelessWidget {
+  const CategoryBodyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NewsByCategoryCubit, NewsByCategoryState>(
+      builder: (context, state) {
+        if (state is FetchNewsByCategorySucess) {
+          if (state.news.isEmpty) {
+            return const NoNewsForThisCategoryViews();
+          }
+          return FetchAllnewsByCategorySucess(news: state.news);
+        } else if (state is FetchNewsByCategoryFailed) {
+          return CustomeFauilerWidget(errorMessage: state.errorMessage);
+        }
+        return const ShimmerNewsByCategoryView();
+      },
     );
   }
 }
---
+++ lib/core/widgets/fauiler_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomeFauilerWidget extends StatelessWidget {
+  final String errorMessage;
+  const CustomeFauilerWidget({super.key, required this.errorMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
